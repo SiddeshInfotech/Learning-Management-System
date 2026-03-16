c38f443ecef000009597c4ef7b8678014e1e5c67
Shivam <shivamtamboli66@gmail.com>
2026-03-16 16:51:00 +0530

    Initial backend setup with Express and TypeScript

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("LMS Backend Running");
+});
+
+export default app;
